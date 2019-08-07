e204390169dbf0f4aac5cb92c4a548b6ce563ec1
Unknown <steven.chou.second@gmail.com>
2019-08-07 15:01:40 +0800

    updated shop data with new data

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // 單純新增一筆(無重複加入購物車)
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
