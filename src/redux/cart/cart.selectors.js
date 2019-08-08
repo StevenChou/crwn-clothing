5d614cee101c45c1fed6a06512d335f9d7c18a8b
Unknown <steven.chou.second@gmail.com>
2019-08-08 13:32:23 +0800

    added checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,12 +1,17 @@
 import { createSelector } from 'reselect';
 
-// 所以傳入的參數為 state
+// 所以傳入的參數為 Redux state
 const selectCart = state => state.cart;
 
 // input selector & output selector
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
 );
 
 // input selector & output selector
@@ -16,6 +21,16 @@ export const selectCartItemsCount = createSelector(
     cartItems.reduce(
       (accumalatedQuantity, cartItem) =>
         accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
       0
     )
 );
